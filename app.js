e584217b0ac9aa92e462a1142bc1e3f4219c9d60
TheBigH50 <HFangmeier@outlook.com>
2022-08-15 08:23:18 -0500

    exe1

--- app.js
+++ app.js
@@ -2,3 +2,13 @@ console.log("Hello World!\n==========\n");
 console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
+let favoriteNumber = 50;
+let answer = prompt("Guess my favorite number!")
+console.log("Hello, "+ answer)
+if (answer < favoriteNumber) {
+  console.log("Too Low")
+} else if (answer > favoriteNumber) {
+  console.log("Too High")
+} else if (answer = favoriteNumber) {
+  console.log("Congratulations!")
+}
