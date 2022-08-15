a4329ba5139d8a87236a32d2afb74ac2d962d462
TheBigH50 <HFangmeier@outlook.com>
2022-08-15 08:43:49 -0500

    exe2

--- app.js
+++ app.js
@@ -3,12 +3,38 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 let favoriteNumber = 50;
-let answer = prompt("Guess my favorite number!")
-console.log("Hello, "+ answer)
+let answer = prompt("Guess my favorite number!");
+console.log("Hello, " + answer);
 if (answer < favoriteNumber) {
-  console.log("Too Low")
+  console.log("Too Low");
 } else if (answer > favoriteNumber) {
-  console.log("Too High")
-} else if (answer = favoriteNumber) {
-  console.log("Congratulations!")
+  console.log("Too High");
+} else if ((answer = favoriteNumber)) {
+  console.log("Congratulations!");
+}
+
+let birthMonth = prompt("What is your birth month?");
+switch (birthMonth) {
+  case "12":
+  case "1":
+  case "2":
+    console.log("Winter");
+    break;
+  case "3":
+  case "4":
+  case "5":
+    console.log("Spring");
+    break;
+  case "6":
+  case "7":
+  case "8":
+    console.log("Summer");
+    break;
+  case "9":
+  case "10":
+  case "11":
+    console.log("Fall");
+    break;
+  default:
+    console.log("Does Not Compute");
 }
